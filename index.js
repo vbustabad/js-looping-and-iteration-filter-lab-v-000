53ae1f5e9c6e9237cc0bdb09cc4bcaa71a8c5c73
Veronica Bustabad <vbustabad@gmail.com>
2018-09-07 02:00:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 // Code your solution in this file
 
 function findMatching(drivers, name) {
-  const newCollection = [];
- 
-  for (const driver of drivers) {
-    if (name(driver)) {
-      newCollection.push(driver);
-    }
-  }
- 
-  return newCollection;
+  const new_drivers_array = drivers.filter(function (name) { return num > 3; });
+  return new_drivers_array;
 }
