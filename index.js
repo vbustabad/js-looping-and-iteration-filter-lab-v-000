9e0116f8014b4d373fcc69449a3dbc6298e3e14f
Veronica Bustabad <vbustabad@gmail.com>
2018-09-07 02:06:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function findMatching(drivers, name) {
   return new_drivers_array;
 }
 
-function fuzzyMatch(drivers, name) {
-  const new_drivers_array = drivers.filter(function (driver) { return driver.toLowerCase() === name.toLowerCase(); });
+function fuzzyMatch(drivers, letters) {
+  const new_drivers_array = drivers.filter(function (driver) { return driver.slice(0, 2) === letters; });
   return new_drivers_array;
 }
