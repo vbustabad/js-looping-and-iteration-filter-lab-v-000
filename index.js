807fb2c16e36ca186c8a716463782e9a52e3e246
Veronica Bustabad <vbustabad@gmail.com>
2018-09-07 01:51:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file
+
+function findMatching(drivers, name) {
+  const newCollection = [];
+ 
+  for (const driver of drivers) {
+    if (name(driver)) {
+      newCollection.push(driver);
+    }
+  }
+ 
+  return newCollection;
+}
